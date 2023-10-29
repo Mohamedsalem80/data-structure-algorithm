4fde73626301955c90aa929e84bdb391dd28682c
Mohamedsalem80 <mohamed3salem1@gmail.com>
2023-10-29 15:11:28 +0200

    searching algorithms

---
+++ search.hpp
@@ -0,0 +1,36 @@
+/**
+ * @brief Linear Search
+ * @attention best: n | average: n | worst: n
+ * 
+ * @tparam T 
+ * @param arr array tempalte pointer
+ * @param n int
+ * @param target template
+ * @return int
+ */
+template <typename T>
+int linearSearch(T arr[], int n, T target) {
+    for (int i = 0; i < n; i++) if (arr[i] == target) return i;
+    return -1;
+}
+
+/**
+ * @brief Binary Search
+ * 
+ * @tparam T 
+ * @param arr array template pointer
+ * @param left int
+ * @param right int
+ * @param target template
+ * @return int
+ */
+template <typename T>
+int binarySearch(T arr[], int left, int right, T target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) return mid;
+        if (arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
