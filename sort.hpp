3b4ba2da403f9b0fd518abbf011825519ade5223
Mohamedsalem80 <mohamed3salem1@gmail.com>
2023-11-01 00:07:11 +0200

    chore: Added Shell Sort

--- sort.hpp
+++ sort.hpp
@@ -182,7 +182,28 @@ void quick_sort(T arr[], int low, int high) {
 }
 
 /**
- * @brief Timsort
+ * @brief Shell Sort
+ * 
+ * @tparam T 
+ * @param arr array T pointer
+ * @param size int
+ */
+template<typename T>
+void shellSort(T arr[], int size) {
+    for (int gap = size / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < size; i++) {
+            T temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+/**
+ * @brief Tim Sort
  * 
  * @tparam T
  * @param arr array T pointer
